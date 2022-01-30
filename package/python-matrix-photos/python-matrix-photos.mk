2a4dcf37a68efa8a9b0c2754f892f29e27e100a7
dirk <info@universalappfactory.de>
2022-01-30 19:13:12 +0100

    fixed wrong matrix-photos version

--- package/python-matrix-photos/python-matrix-photos.mk
+++ package/python-matrix-photos/python-matrix-photos.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_MATRIX_PHOTOS_VERSION = 0.0.2
+PYTHON_MATRIX_PHOTOS_VERSION = 0.0.3
 PYTHON_MATRIX_PHOTOS_SOURCE = matrix-photos-$(PYTHON_MATRIX_PHOTOS_VERSION).tar.gz
 PYTHON_MATRIX_PHOTOS_SITE = https://files.pythonhosted.org/packages/b2/70/267b57ee783cbbc4f268ad9ac208140459b5d97ddec736c6ed1b928f4ab5
 
