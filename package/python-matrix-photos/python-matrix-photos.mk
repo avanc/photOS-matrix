acdc252b2b869ca5c932b1212f8f61784d6d569d
dirk <info@universalappfactory.de>
2022-01-30 20:01:53 +0100

    fixed package link

--- package/python-matrix-photos/python-matrix-photos.mk
+++ package/python-matrix-photos/python-matrix-photos.mk
@@ -6,7 +6,8 @@
 
 PYTHON_MATRIX_PHOTOS_VERSION = 0.0.3
 PYTHON_MATRIX_PHOTOS_SOURCE = matrix-photos-$(PYTHON_MATRIX_PHOTOS_VERSION).tar.gz
-PYTHON_MATRIX_PHOTOS_SITE = https://files.pythonhosted.org/packages/b2/70/267b57ee783cbbc4f268ad9ac208140459b5d97ddec736c6ed1b928f4ab5
+PYTHON_MATRIX_PHOTOS_SITE = https://files.pythonhosted.org/packages/99/8c/15d3b75812bb3e08bc961917e7eb1f0053aedf0fbd52bfec47f80416846f
+
 
 PYTHON_MATRIX_PHOTOS_SETUP_TYPE = setuptools
 
